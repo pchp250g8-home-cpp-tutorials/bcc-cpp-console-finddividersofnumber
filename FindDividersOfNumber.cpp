66cbd94215bdb66a301c59ad2d0637f5eb89b369
Alexander <pchp250g8@tutamail.com>
2026-03-28 16:51:42 +0300

    Fixed Source Code

--- FindDividersOfNumber.cpp
+++ FindDividersOfNumber.cpp
@@ -6,7 +6,7 @@ int _tmain(int argc, _TCHAR* argv[])
    unsigned long ulNumber;
    std::cout << "Input an integer positive number\r\n";
    std::cin >> ulNumber;
-   std::cout << "The number " << ulNumber << " has the folowing dividers:\r\n";
+   std::cout << "The number " << ulNumber << " has the folowing divisors:\r\n";
    for (unsigned long i = 1; i <= ulNumber; i++)
    {
      if (ulNumber % i == 0)
